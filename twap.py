4f854bbdc8aedf1c2ec73c37331d4f73b39baffa
J. Chitty <jesuschitty7@hotmail.com>
2022-06-07 12:03:42 +0200

    Update twap.py

--- twap.py
+++ twap.py
@@ -9,10 +9,14 @@ api_key= ''
 api_secret= ''
 client = Client(api_key, api_secret)
 
-# Symbol you wish to trade
-
+# Symbol you wish to trade and price
 symbol = 'BNBUSDT'
 price = client.get_avg_price(symbol=symbol)
+
+# initialize bot lol
+print('Copernico TWAP BOT initializing...')
+time.sleep(1)
+print('{} Price: {}'.format(symbol, price['price']))
 
 ## TWAP BUY
 # HOW MUCH OF THE ASSET YOU WISH TO BUY
@@ -24,39 +28,12 @@ def job():
     order_buy = client.create_order(symbol=symbol, side=Client.SIDE_BUY, type=Client.ORDER_TYPE_MARKET, quantity=quantity)
     print(order_buy)
 
-#run every minute
-#schedule.every(1).minutes.do(job)
-
 # Schedule a job to run for the next 24 hours
 schedule.every(1).hours.until(timedelta(hours=24)).do(job)
 
+all_jobs = schedule.get_jobs()
+print(all_jobs)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-
-## schedule examples
-#schedule.every(10).seconds.do(job)
-#schedule.every(10).minutes.do(job)
-#schedule.every().hour.do(job)
-#schedule.every().day.at("10:30").do(job)
-#schedule.every(5).to(10).minutes.do(job)
-#schedule.every().monday.do(job)
-#schedule.every().wednesday.at("13:15").do(job)
-#schedule.every().minute.at(":17").do(job)
-
-
-# run job until a 18:30 today
-#schedule.every(1).hours.until("18:30").do(job)
-
-# run job until a 2030-01-01 18:33 today
-#schedule.every(1).hours.until("2030-01-01 18:33").do(job)
-
-# Schedule a job to run for the next 8 hours
-#schedule.every(1).hours.until(timedelta(hours=8)).do(job)
-
-# Run my_job until today 11:33:42
-#schedule.every(1).hours.until(time(11, 33, 42)).do(job)
-
-# run job until a specific datetime
-#schedule.every(1).hours.until(datetime(2020, 5, 17, 11, 36, 20)).do(job)
